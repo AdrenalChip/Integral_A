7022103e627f446f6480998eb3e13e833969d976
Jorge Guerrero Díaz <74365148+AdrenalChip@users.noreply.github.com>
2021-11-01 09:09:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -58,6 +58,7 @@ int agregar_libro(Libro oc[],int y){
      for(int i=y; i < z; i++){ 
          oc[i].set_name();
          oc[i].set_pages();
+         oc[i].set_cantidad();
          cout<<"Cuantas categorias agregaras? (Numero)";
          int num;
          cin>>num;
@@ -113,7 +114,7 @@ return y;
  * @return
 */
 void categoria(Libro oc[],int y){
-    cout<<"Escoge categoria (Escribe el nombre)\n";
+    cout<<"Escoge categoria (Escribe el nombre, 1 solo una a la vez)\n";
     cout<<"Fantasia,Misterio,Magia,Adolescentes,Mitologia,Ficcion,Accion,Aventura,Novela,Religion,Thriller";
     string x;
     cin>>x;
@@ -132,7 +133,7 @@ void categoria(Libro oc[],int y){
 void catalogo(Libro oc[],int y){
     int flag=0;
     while (flag==0){
-        cout<<"Welcome to Catalog\n Que desea hacer? \n 1.-Imprimir libros \n 2.-Ordenar por paginas \n 3.-Buscar por categoria \n 4.-Salir del Catalogo \n";
+        cout<<"Welcome to Catalog\n Que desea hacer? \n 1.-Imprimir libros \n 2.-Ordenar por paginas \n 3.-Ordenar por cantidad en stock \n 4.-Buscar por categoria \n 5.-Salir del Catalogo \n";
        int x;
        cin>>x;
        if (x==1){
@@ -151,14 +152,52 @@ void catalogo(Libro oc[],int y){
         sorts.ordenaBurbuja(original1);
         arreglararray(original1,oc,y);
        }else if (x==3){
+           int x=cantidad_tree(oc,y);
+        }else if (x==4){
            categoria(oc,y);
-       }else if(x==4){
+       }else if(x==5){
            flag=1;
        }else{
            cout<< "Opcion Invalidad,ingrese nuevamente";
        }
    }
 }
+
+int cantidad_tree(Libro oc[],int y){
+    BST<int> bst;
+    //generate bst
+    int max=oc[0].get_cantidad();
+    int ct=oc[0].get_cantidad();
+    for (int i=0;i<y;i++){
+        if (max<oc[i].get_cantidad()){
+            max=oc[i].get_cantidad();
+        }else if (max>oc[i].get_cantidad()){
+        }
+        ct=oc[i].get_cantidad();
+        bst.add(ct);
+        }
+    bool x=bst.find(max);
+    if (x){
+        for (int i=0;i<y;i++){
+            if (max==oc[i].get_cantidad()){
+                cout<<"The books with more copies is "<<oc[i].get_name()<<"\n";
+            }
+        }
+    }
+    int *orden;
+    orden=bst.inorder();
+    cout<<"Orden de menor a mayor cantidad ";
+    for (int i=0;i<y;i++){
+        for (int j=0;j<y;j++){
+            if (oc[j].get_cantidad()==*(orden+i)){
+                cout<<oc[j].get_name()<<" ";
+            }
+        }
+        cout<<*(orden+i);
+    }
+    return y;
+}
+
 /*
  * inicializar
  * Funcion que inicializa el arreglo de libros, para que 
@@ -169,37 +208,42 @@ void catalogo(Libro oc[],int y){
 int inicializar(Libro oc[],int y){
      oc[0].set_names("HarryPotter");
      oc[0].set_paginas(223);
+     oc[0].set_cantidads(10);
      string cats0[4]={"Fantasia","Magia","Adolescentes",};
      oc[0].set_categoria(cats0,4);
     
      oc[1].set_names("PercyJackson");
      oc[1].set_paginas(377);
+     oc[1].set_cantidads(5);
      string cats1[4]={"Fantasia","Magia","Adolescentes","Mitologia"};
      oc[1].set_categoria(cats1,4);
 
      oc[2].set_names("HungerGames");
      oc[2].set_paginas(374);
+     oc[2].set_cantidads(12);
      string cats2[3]={"Ficcion","Adolescentes","Accion"};
      oc[2].set_categoria(cats2,3);
 
      oc[3].set_names("ElSalvaje");
      oc[3].set_paginas(696);
+     oc[3].set_cantidads(7);
      string cats3[3]={"Ficcion","Novela","Aventura"};
      oc[3].set_categoria(cats3,3);
 
      oc[4].set_names("Bible");
      oc[4].set_paginas(1200);
+     oc[4].set_cantidads(2);
      string cats4[1]={"Religion"};
      oc[4].set_categoria(cats4,1);
 
      oc[5].set_names("TheAnalyst");
      oc[5].set_paginas(521);
+     oc[5].set_cantidads(9);
      string cats5[3]={"Ficcion","Thriller","Magia"};
      oc[5].set_categoria(cats5,3);
-    
-     
 return y;
 }
+
 /*
  * main
  * Menu de donde se llaman todas las funciones
@@ -221,6 +265,8 @@ int main(){
            tamano=agregar_libro(oc,tamano);
        }else if(x==3){
            tamano=eliminar_libro(oc,tamano);
+       }else if (x==4){
+           tamano=cantidad_tree(oc,tamano);
        }else if (x==4){
         flag=1;
        }else{
